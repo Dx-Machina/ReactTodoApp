5c3c1dba96bd6ce02d55fd9b92b720e442b5a63d
Youssef Hegazy <You7Joe@iCloud.com>
2024-04-25 09:46:43 -0700

    in-class update (wed 24th 2024)

--- TodoApp/src/Components/TodoItem.jsx
+++ TodoApp/src/Components/TodoItem.jsx
@@ -1,3 +1,13 @@
-export default function TodoItem({ todo }) {
-  return <h2 key={todo}>{todo}</h2>;
+import style from ",/TodoItem.module.css";
+export default function TodoItem({ todo }) 
+{
+  function handleClick(todo)
+  {
+    console.log("Delte button Clicked", todo);
+  }
+  return
+  (<div>
+    <h2 key={todo}>{todo}</h2>;
+    <span><button onClick={()=>handleClick(todo)}>x</button></span>
+  </div>);
 }
